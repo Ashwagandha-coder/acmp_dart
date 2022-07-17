9990b8cc6333c9e1a6029364b93bdb51352e50a6
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-17 13:00:30 +0500

    Initial commit

---
+++ bin/acmp_dart.dart
@@ -0,0 +1,5 @@
+import 'package:acmp_dart/acmp_dart.dart' as acmp_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${acmp_dart.calculate()}!');
+}
---
+++ test/acmp_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:acmp_dart/acmp_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
